cfde3dd8e9b79bfdffa0f9cc8fc2fc147e1f08a5
ThegreatHamza <hamzamohamedmoud@gmail.com>
2026-09-07 14:52:50 +0300

    feat: register service worker

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import {createRoot} from 'react-dom/client'
 import {App} from './App'
 import './styles.css'
+if('serviceWorker' in navigator) window.addEventListener('load',()=>navigator.serviceWorker.register('/sw.js').catch(()=>{}))
 createRoot(document.getElementById('root')!).render(<App/>)
